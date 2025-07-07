8c192549dcc0a23ba0f329e4e9441420d85cfe53
Alvarog0218 <122694377+Alvarog0218@users.noreply.github.com>
2025-07-07 11:26:32 -0500

    limpiar componentes y apis eliminados

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -3,7 +3,7 @@
 import { useContext } from 'react';
 import { LanguageContext } from '../contexts/LanguageContext';
 import { termsContent } from '../lib/termsContent';
-import ContactForm from './ContactForm';
+//import ContactForm from './ContactForm';
 
 export default function ContactPage() {
   const { language } = useContext(LanguageContext);
@@ -20,7 +20,7 @@ export default function ContactPage() {
         className="w-screen h-[calc(100vh-120px)] mt-[120px] flex items-start justify-center overflow-y-auto bg-black custom-scroll"
       >
         <div className="text-left text-white w-[900px] py-4 px-4">
-          <ContactForm />
+          
         </div>
       </section>
     </div>
